6a90208997612e89ae27da10d8cc14e50a567c77
mFaYizp <fayizfaz78@gmail.com>
2023-09-24 22:40:23 +0530

    setting Auth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import { ConnectToDB } from "@utils/database";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      await ConnectToDB()
+
+      return true
+    } catch (error) {
+      console.log(error);
+      return false
+    }
+  },
+});
+export { handler as GET, handler as POST };
