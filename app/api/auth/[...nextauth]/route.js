910adbd95f7b84a75ff8500ccac2b3cbaef2a5b8
mFaYizp <fayizfaz78@gmail.com>
2023-09-25 08:46:11 +0530

    homePage,auth setted

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
+import User from "@models/user";
 import { ConnectToDB } from "@utils/database";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
@@ -9,16 +10,40 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      await ConnectToDB()
+  callbacks: {
+    async session({ session }) {
+      const sessionUsers = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUsers._id.toString();
 
-      return true
-    } catch (error) {
-      console.log(error);
-      return false
-    }
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        await ConnectToDB();
+
+        //CHECKING CURRENT USERS
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        //CREATING NEW USER
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,17 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession()
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -33,7 +33,7 @@ const Nav = () => {
       </Link>
       {/* DESKTOP NAVIGATION */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -43,7 +43,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -69,10 +69,10 @@ const Nav = () => {
       </div>
       {/*  MOBILE NAVIGATION */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
